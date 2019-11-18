b955aa9e81d2b3a38dd12137c166b988b8b6013e
GONRODRIGUEZ <gonzalo.grodriguez@mercadolibre.com>
2019-11-18 18:32:16 -0300

    Algunos cambios turbios

--- TP3/ejercicio4/proceso1.c
+++ TP3/ejercicio4/proceso1.c
@@ -5,25 +5,24 @@
 #include <sys/shm.h>
 #include <semaphore.h>
 #include <fcntl.h>
- #include <sys/types.h>
- #include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/mman.h>
 #include <unistd.h>
-#include <sys/mman.h> 
-#include <sys/stat.h> 
+#include <sys/mman.h>
+#include <sys/stat.h>
 
 
-int main (int arg,char *args[]){
-  int fd;
-  int *mem;
-  size_t tam=sizeof(int);
-  sent_t *sem= sem_open("semaforo",O_CREAT,0600,0);
+int main(int arg, char *args[]) {
+    int fd;
+    size_t tam = sizeof(int);
+    sem_t *sem = sem_open("semaforo", O_CREAT, 0600, 0);
 
-  fd=shm_open("/nombre",O_CREAT,0600);
-  ftruncate(fd,tam);
-  //mem es la memoria compartida, si cambio su valor estoy escribiendo en
-  //la mem compartida, si veo su valor estoy leyendo de memoria compartida
-  mem=mmap(NULL,tam,PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
-    mem=5;
+    fd = shm_open("/nombre", O_CREAT, 0600);
+    ftruncate(fd, tam);
+    //mem es la memoria compartida, si cambio su valor estoy escribiendo en
+    //la mem compartida, si veo su valor estoy leyendo de memoria compartida
+    uint8_t *mem = mmap(NULL, tam, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, fd, 0);
+    *mem =  5;
     sem_post(sem); //V()
-  close(fd) ; 
+    close(fd);
 }
--- TP3/ejercicio4/proceso2.c
+++ TP3/ejercicio4/proceso2.c
@@ -1,18 +1,24 @@
-int main (int arg,char *args[]){
-    int fd;
-  int *mem;
-  size_t tam=sizeof(int);
-  sent_t *sem= sem_open("semaforo",O_CREAT,0600,0);
+#include <stdio.h>
+#include <stddef.h>
+#include <semaphore.h>
+#include <sys/mman.h>
+#include <zconf.h>
+#include <stdint.h>
 
-  fd=shm_open("/nombre",O_CREAT,0600);
-  ftruncate(fd,tam);
-  //mem es la memoria compartida, si cambio su valor estoy escribiendo en
-  //la mem compartida, si veo su valor estoy leyendo de memoria compartida
-  mem=mmap(NULL,tam,PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
+int main(int arg, char *args[]) {
+    int fd;
+    size_t tam = sizeof(int);
+    sem_t *sem = sem_open("semaforo", O_CREAT, 0600, 0);
+
+    fd = shm_open("/nombre", O_CREAT, 0600);
+    ftruncate(fd, tam);
+    //mem es la memoria compartida, si cambio su valor estoy escribiendo en
+    //la mem compartida, si veo su valor estoy leyendo de memoria compartida
+    uint8_t *mem = mmap(NULL, tam, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, fd, 0);
     sem_wait(sem); // P()
-    pritf("valor: %d",mem);
+    printf("valor: %d", *mem);
     sem_close(sem);
     sem_unlink("semaforo");
-  close(fd) ; 
-    
+    close(fd);
+
 }
